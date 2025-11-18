065f8ebae22eadbc97949c1d3673da2bea659f0e
Azlan Jamshed <152954543+azlanjamshed@users.noreply.github.com>
2025-11-19 00:19:17 +0530

    update project

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -2,25 +2,35 @@ import todo from "../assets/todo.png"
 import weather from "../assets/weather.png"
 import password from "../assets/password.png"
 import gsla from "../assets/gsla.png"
+import mfc from "../assets/MFC.png"
 const projects = [
     {
         image: gsla,
         title: "Global Service and Legal associates ",
         type: "Frontend",
-        description: "",
+        description: "A complete frontend website for a legal and service consultancy firm, built with React and Tailwind CSS. Includes structured service pages, contact section, responsive layouts, and SEO-friendly design.",
         techStack: ["React", "Tailwind CSS"],
         link: "https://gslassociates.in",
         gitLink: "https://github.com/azlanjamshed/GLOBAL-SERVICES-LEGAL-ASSOCIATES",
     },
     {
-        image: todo,
-        title: "ToDo App",
+        image: mfc,
+        title: "MSL 2025 Football League",
         type: "Frontend",
-        description: "Remove image backgrounds using React + Express + AI API.",
-        techStack: ["React", "Tailwind CSS", "Local Storage"],
-        link: "https://todo-aj.netlify.app",
-        gitLink: "https://github.com/azlanjamshed/To-Do",
+        description: "A responsive football league standings and match-center app built using React and Tailwind CSS, featuring dynamic team data, auto-computed points table, and clean UI with smooth interactions.",
+        techStack: ["React", "Tailwind CSS"],
+        link: "https://msl-2025-gen2de.netlify.app",
+        gitLink: "https://github.com/azlanjamshed/MFC",
     },
+    // {
+    //     image: todo,
+    //     title: "ToDo App",
+    //     type: "Frontend",
+    //     description: "Remove image backgrounds using React + Express + AI API.",
+    //     techStack: ["React", "Tailwind CSS", "Local Storage"],
+    //     link: "https://todo-aj.netlify.app",
+    //     gitLink: "https://github.com/azlanjamshed/To-Do",
+    // },
     {
         image: weather,
         title: "Weather App",
